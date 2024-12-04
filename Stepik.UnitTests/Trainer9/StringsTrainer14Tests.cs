39c00edcdc20bd0398e34acdc13743dcc92bfd83
Anastasia Kuznetsova <www.Kuzya-Nastya@mail.ru>
2024-12-04 21:34:56 +0300

    Степик 9.1.14
    Добавлено решение задачи на степике из вкладки 9.1 - 14я задача. Также добавлены модульные тесты на эту задачу.

---
+++ Stepik.UnitTests/Trainer9/StringsTrainer14Tests.cs
@@ -0,0 +1,23 @@
+﻿using Stepik.Trainer9;
+
+namespace Stepik.UnitTests.Trainer9;
+
+public class StringsTrainer14Tests
+{
+    private readonly StringsTrainer14 _sut = new();
+
+    [Fact]
+    public void WhenMessageAndReplacingSubstringIsValid_ShouldReturnReplaceString()
+    {
+        // arrange
+        const string replace = "бананы";
+        const string message = "Я люблю яблоки.";
+        const string expected = "Я люблю бананы.";
+
+        // act
+        var result = _sut.ReplaceSubstringInMessage(replace, message);
+
+        // assert
+        result.Should().Be(expected);
+    }
+}
---
+++ Stepik/Trainer9/StringsTrainer14.cs
@@ -0,0 +1,25 @@
+﻿namespace Stepik.Trainer9;
+
+public class StringsTrainer14
+{
+    /**
+     * <description>
+     * <see href="https://stepik.org/lesson/1279864/step/13?auth=registration&unit=1294245"/>
+     * У вас есть переменные replace, message которые содержат входные пользовательские данные.
+     * Напишите код, который заменяет в строке message слово яблоки на значение переменной replace
+     * и записывает результат в переменную result.
+     * </description>
+     * <example><para>Trainer14("бананы", "Я люблю яблоки.")
+          -> "Я люблю бананы."</para></example>
+     * <summary>
+     * Заменяет подстроку в строке.
+     * </summary>
+     * <param name="replace">Строка, по которой необходимо выполнить поиск.</param>
+     * <param name="message">Строка, в которой будет производиться поиск.</param>
+     * <returns>Возвращает строку message c замененной подстрокой replace.</returns>
+     **/
+    public string ReplaceSubstringInMessage(string replace, string message)
+    {
+        return message.Replace("яблоки",replace);
+    }
+}
