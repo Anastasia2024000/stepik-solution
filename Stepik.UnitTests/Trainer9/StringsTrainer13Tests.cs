0d6560f30a00117d09ac763e0afa2956f1908de1
Anastasia Kuznetsova <www.Kuzya-Nastya@mail.ru>
2024-12-04 20:43:09 +0300

    Степик 9.1.13
    Добавлено решение задачи на степике из вкладки 9.1 - 13я задача. Также добавлены модульные тесты на эту задачу.

---
+++ Stepik.UnitTests/Trainer9/StringsTrainer13Tests.cs
@@ -0,0 +1,23 @@
+﻿using Stepik.Trainer9;
+
+namespace Stepik.UnitTests.Trainer9;
+
+public class StringsTrainer13Tests
+{
+    private readonly StringsTrainer13 _sut = new();
+
+    [Fact]
+    public void WhenSymolsIsExistsInString_ShouldReturnExpectedString()
+    {
+        // arrange
+        const string search = "Ривенделл";
+        const string expected = "р:*,и:*,в:*,е:**,н:*,д:*,л:**";
+
+        // act
+        var result = _sut.FindRepeatingOfSymbols(search);
+
+        // assert
+        result.Should().Be(expected);
+    }
+
+}
---
+++ Stepik/Trainer9/StringsTrainer13.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Authentication;
+
+namespace Stepik.Trainer9;
+
+public class StringsTrainer13
+{
+    /**
+    * <description>
+    * <see href="https://stepik.org/lesson/1279864/step/14?auth=registration&unit=1294245"/>
+    * У вас есть переменная city, которая содержит входные пользовательские данные.
+    * Напишите код, который подсчитывает сколько раз встречается буква в строке city,
+    * отображает количество повторений в виде звездочек * и записывает результат в переменную result.
+    * </description>
+    * <example><para>Trainer13("Ривенделл")
+         -> "р:*,и:*,в:*,е:**,н:*,д:*,л:**" </para></example>
+    * <summary>
+    * Находит количество повторений каждого символа в строке.
+    * </summary>
+    * <param name="city">Строка, по которой необходимо выполнить поиск.</param>
+    * <returns>Возвращает количество повторений каждого символа в строке.</returns>
+    **/
+    public string FindRepeatingOfSymbols(string city)
+    {
+        var groupBy = city.ToLower().GroupBy(x => x,
+            (c, chars) => $"{c}:{new string('*', chars.Count())}"
+        );
+
+        return string.Join(',', groupBy);
+    }
+}
